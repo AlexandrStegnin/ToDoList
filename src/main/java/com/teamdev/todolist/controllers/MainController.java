5703ed7480f1c8d1339c52d3f92bed899c7e5f8f
Alexandr Stegnin <alexandr.stegnin@gmail.com>
2019-03-14 14:33:03 +0500

    Создан контроллер, который переадресовывает на автосгенерированную страницу swagger

---
+++ src/main/java/com/teamdev/todolist/controllers/MainController.java
@@ -0,0 +1,24 @@
+package com.teamdev.todolist.controllers;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@Controller
+@RequestMapping("/")
+@Api(value = "MainController", description = "Redirect to swagger-ui when open default app path (\"/\")")
+public class MainController {
+
+    @GetMapping
+    @ApiOperation(value = "Go to swagger ui page", response = String.class)
+    public String swaggerUiPage() {
+        return "redirect:/swagger-ui.html";
+    }
+
+}
