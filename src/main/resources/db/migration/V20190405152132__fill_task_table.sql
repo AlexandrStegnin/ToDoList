8ea1a8641b972ceba0b6d6a3f1c49e81e2da72a9
Alexandr Stegnin <alexandr.stegnin@gmail.com>
2019-04-05 15:29:43 +0500

    Добавлены скрипты изменения БД. Заполнить sequences для сущности team. Добавить задачи в таблицу.

---
+++ src/main/resources/db/migration/V20190405152132__fill_task_table.sql
@@ -0,0 +1,9 @@
+INSERT INTO task
+(id, title, description, author_id, creation_date, execution_date, status_id, parent_task_id, created, updated, comment, workspace_id)
+VALUES(2, 'Название', 'Описание', 1, '2019-04-05', '2019-04-06', 1, NULL, '2019-04-05 04:16:43.000', '2019-04-05 04:16:43.000', '', 1);
+INSERT INTO task
+(id, title, description, author_id, creation_date, execution_date, status_id, parent_task_id, created, updated, comment, workspace_id)
+VALUES(3, 'Название 2', 'Описание 2', 1, '2019-04-05', '2019-04-06', 1, NULL, '2019-04-05 04:18:02.000', '2019-04-05 04:18:02.000', '', 1);
+INSERT INTO task
+(id, title, description, author_id, creation_date, execution_date, status_id, parent_task_id, created, updated, comment, workspace_id)
+VALUES(4, 'Название', 'Описание', 1, '2019-04-05', '2019-04-06', 1, NULL, '2019-04-05 06:52:52.000', '2019-04-05 06:52:52.000', '', 1);
