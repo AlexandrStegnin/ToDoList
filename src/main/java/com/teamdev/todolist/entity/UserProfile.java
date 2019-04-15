26bb771879678d65048af582bf2ea95f7c6b3a18
Alexandr Stegnin <alexandr.stegnin@gmail.com>
2019-04-15 17:02:44 +0500

    Добавлена валидация полей профиля пользователя.

--- src/main/java/com/teamdev/todolist/entity/UserProfile.java
+++ src/main/java/com/teamdev/todolist/entity/UserProfile.java
@@ -8,7 +8,9 @@ import lombok.ToString;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -36,11 +38,13 @@ public class UserProfile extends AbstractEntity {
 
     @Column
     @NotNull(message = "Необходимо указать имя")
+    @Size(min = 1, max = 100, message = "Имя должно быть от 1 до 100 символов")
     @ApiModelProperty(notes = "User name")
     private String name;
 
     @Column
     @NotNull(message = "Необходимо указать фамилию")
+    @Size(min = 1, max = 100, message = "Фамилия должна быть от 1 до 100 символов")
     @ApiModelProperty(notes = "User surname")
     private String surname;
 
@@ -50,7 +54,9 @@ public class UserProfile extends AbstractEntity {
 
     @Email
     @Column
+    @NotBlank(message = "Необходимо указать email")
     @NotNull(message = "Необходимо указать email")
+    @Size(min = 6, message = "Необходимо указать корректный email")
     @ApiModelProperty(notes = "User email")
     private String email;
 
