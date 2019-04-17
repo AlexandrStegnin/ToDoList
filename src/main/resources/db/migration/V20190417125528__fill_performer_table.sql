551c8c60025af3a71a89c4a34f3fc9d776d08fa7
Alexandr Stegnin <alexandr.stegnin@gmail.com>
2019-04-17 13:54:05 +0500

    Добавлены скрипты изменения БД. аполняем таблицу исполнители и тэги задач.

---
+++ src/main/resources/db/migration/V20190417125528__fill_performer_table.sql
@@ -0,0 +1,3 @@
+INSERT INTO todo_list_db.performer (task_id, performer_id) VALUES (2, 1);
+INSERT INTO todo_list_db.performer (task_id, performer_id) VALUES (3, 1);
+INSERT INTO todo_list_db.performer (task_id, performer_id) VALUES (4, 1);
---
+++ src/main/resources/db/migration/V20190417125803__fill_table_task_tag.sql
@@ -0,0 +1,4 @@
+INSERT INTO todo_list_db.task_tag (task_id, tag_id) VALUES (2, 3);
+INSERT INTO todo_list_db.task_tag (task_id, tag_id) VALUES (3, 3);
+INSERT INTO todo_list_db.task_tag (task_id, tag_id) VALUES (4, 3);
+INSERT INTO todo_list_db.task_tag (task_id, tag_id) VALUES (4, 2);
